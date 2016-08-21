0d90ef2b3a05692828344a6d41c40564d7dad9f2
cowry <cwryhs@gmail.com>
2016-08-21 23:23:30 +0000

    connection test 4

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ io.serveClient(false);
 
 io.on("connection", function (socket){
     console.log("client connected");
-    socket.emit("connection test", {message : "this is a message", num : 2.5, arr : [0, 1, 2], jaggedArr : [0.5, "one point five", 2.5]});
+    socket.emit("connection test", {message : "this is a message", num : 2.5, arr : [0, 1, 2], jaggedArr : [0.5, "one point five", 2.5], objArray : [{message : "first sub message"}, {message : "second sub message"}, {message : "third sub message"}]});
 })
 
 server.listen(process.env.PORT);
