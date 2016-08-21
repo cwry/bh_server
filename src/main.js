ac2678a833c3f957a2e9626b032954ff6b267cfc
cowry <cwryhs@gmail.com>
2016-08-21 22:48:27 +0000

    connection test

--- src/main.js
+++ src/main.js
@@ -3,15 +3,15 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
 app.get("/", function(req, res){
-    console.log("greeted");
+    console.log("http request");
     res.send("greetings!");
 });
 
 io.serveClient(false);
 
 io.on("connection", function (socket){
-    console.log("greeted socket");
-    socket.emit("connection test");
+    console.log("client connected");
+    socket.emit("connection test", {message : "this is a message", num : 2.5});
 })
 
 server.listen(process.env.PORT);
