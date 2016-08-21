d3e52d47943c311fc75eeb9037825d97b09c1fff
cowry <cwryhs@gmail.com>
2016-08-21 21:44:58 +0000

    removed gulp, added socket hello world

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-const express = require("express");
-
-const app = express();
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
 app.get("/", function(req, res){
     console.log("greeted");
     res.send("greetings!");
 });
 
-app.listen(process.env.PORT);
+io.serveClient(false);
+
+io.on("connection", function (socket){
+    console.log("greeted socket");
+    socket.emit("connection test");
+})
+
+server.listen(process.env.PORT);
